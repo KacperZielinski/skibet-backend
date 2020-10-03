e70e52a12b2331c02270cfbb05192f94f197ed22
Kacper Zielinski <kacper.zielinski@mailfence.com>
2020-10-03 09:44:34 +0200

    Initial commit

---
+++ src/main/java/pl/kz/skibet/SkibetBackendApplication.java
@@ -0,0 +1,13 @@
+package pl.kz.skibet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkibetBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SkibetBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/kz/skibet/SkibetBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.kz.skibet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkibetBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
