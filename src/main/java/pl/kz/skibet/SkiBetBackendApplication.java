ca63eefc39f723af605eb875d61ec61b463d5fbc
Kacper Zielinski <kacper.zielinski@mailfence.com>
2020-10-03 09:47:56 +0200

    Added SSL support for production backend

--- src/main/java/pl/kz/skibet/SkiBetBackendApplication.java
+++ src/main/java/pl/kz/skibet/SkiBetBackendApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SkibetBackendApplication {
+public class SkiBetBackendApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SkibetBackendApplication.class, args);
+        SpringApplication.run(SkiBetBackendApplication.class, args);
     }
 
 }
