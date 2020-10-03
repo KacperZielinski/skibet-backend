729fe7f1491ae8e092983283102b298d58f1b29e
Kacper Zielinski <kacper.zielinski@mailfence.com>
2020-10-03 17:47:27 +0200

    Removed redirection from HTTP to HTTPS (useless now)

--- src/main/java/pl/kz/skibet/rest/HelloController.java
+++ src/main/java/pl/kz/skibet/rest/HelloController.java
@@ -1,13 +1,19 @@
 package pl.kz.skibet.rest;
 
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Collections;
+import java.util.Map;
+
 @RestController
+@CrossOrigin
 public class HelloController {
 
-    @GetMapping
-    public String hello() {
-        return "Elooo elo 320 !";
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public Map<String, String> hello() {
+        return Collections.singletonMap("response", "NonDB - Elooo elo 320 !");
     }
 }
