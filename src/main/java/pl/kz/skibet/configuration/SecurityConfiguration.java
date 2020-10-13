4ebd7750b4fc3d89adf4ad664740d9a2df3aa486
Kacper Zielinski <kacper.zielinski@mailfence.com>
2020-10-13 15:41:54 +0200

    Making REST API session stateless

--- src/main/java/pl/kz/skibet/configuration/SecurityConfiguration.java
+++ src/main/java/pl/kz/skibet/configuration/SecurityConfiguration.java
@@ -6,6 +6,7 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 
 @Configuration
 @EnableWebSecurity
@@ -21,10 +22,14 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
+        http
+                .authorizeRequests()
                 .anyRequest().authenticated()
                 .and()
                 .httpBasic()
+                .and()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
                 .cors();
     }
